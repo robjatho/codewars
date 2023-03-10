9a8b23eeff18051b3e78850ad0a9c866dc9a6a04
Rob Jatho <robertj@m4jam.com>
2023-03-10 11:20:41 +0200

    Sped up cube build

--- python/build_a_pile_of_cubes/build_a_pile_of_cubes.py
+++ python/build_a_pile_of_cubes/build_a_pile_of_cubes.py
@@ -12,6 +12,31 @@ def find_nb_slow(m):
     return -1
 
 
+def find_nb(m):
+    n=1
+    end_result = 0
+    top_result = 0
+    while n > 0:
+        if m - end_result == 0:
+            return n
+        #elif end_result > m:
+        #    top_result = n
+        if n > top_result:
+            n = n*2
+        else:
+            n = n + 1
+            if n == top_result:
+                return -1
+        end_result = n**3
+        for item in reversed(range(1, n+1)):
+            end_result += (n-item)**3
+            if end_result > m:
+                top_result = n
+                n = n - item
+                break
+    return -1
+
 if __name__ == '__main__':
     print(find_nb(1071225))
     print(find_nb(4183059834009))
+    print(find_nb(24723578342962))
